179ed4586d574832fca5eda498db91854c168a8c
benjbwr <benjbwr@unm.edu>
2021-04-10 20:35:13 -0600

    updated scheduler.py again

--- scheduler.py
+++ scheduler.py
@@ -1,44 +1,42 @@
 import numpy as np
 import calendar as cal
 
-class cal7:
-    week = [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
 
-caltest = cal7()
-
-caltest.week[0][0] = 'Ben'
-caltest.week[0][1] = 'Alex'
-
-print(caltest.week[0])
+class Cal7:
+    week_am = np.zeros((7,), dtype=int)
+    week_pm = np.zeros((7,), dtype=int)
 
 
-class provider:
+class Provider:
     def __init__(self, provider_name, specialty, clinic_preference, am_day_preferences, pm_day_preferences, days_off,
                  total_available_hours):
-
         self.provider_name = provider_name
-        self.specialty = specialty                        # [Child,Adult,Family] Preferences will be indicated by 1
-        self.clinic_preference =  [0,0,0]                 # [Clinic1,Clinic2,Clinic3] Preference will be indicated by 1
-        self.am_day_preferences = [0,0,0,0,0,0,0]                   # [M,T,W,T,F,S,S] Preference will be indicated by 1
-        self.pm_day_preferences = [0,0,0,0,0,0,0]                   # [M,T,W,T,F,S,S] Preference will be indicated by 1
-        self.days_off = [0,0,0,0,0,0,0]                             # [M,T,W,T,F,S,S] Preference will be indicated by 1
-        self.total_available_hours = 40
-
-class clinic:
-
-    def __init__(self,clinic_name, min_staff, optimal_staff, max_staff):
-
-        self.clinic_name = 'Name'
-        self.min_staff = 0
-        self.optimal_staff = 0
-        self.max_staff = 0
+        self.specialty = specialty  # [Child,Adult,Family] Preferences will be indicated by 1
+        self.clinic_preference = clinic_preference  # [Clinic1,Clinic2,Clinic3] Preference will be indicated by 1
+        self.am_day_preferences = am_day_preferences  # [M,T,W,T,F,S,S] Preference will be indicated by 1
+        self.pm_day_preferences = pm_day_preferences  # [M,T,W,T,F,S,S] Preference will be indicated by 1
+        self.days_off = days_off  # [M,T,W,T,F,S,S] Preference will be indicated by 1
+        self.total_available_hours = total_available_hours
 
 
-p1 = provider('john',[0,1,0])
+class Clinic:
+    def __init__(self, clinic_name, min_staff, optimal_staff, max_staff):
+        self.clinic_name = clinic_name
+        self.min_staff = min_staff
+        self.optimal_staff = optimal_staff
+        self.max_staff = max_staff
 
 
+# caltest = cal7()
+
+# caltest.week[0][0] = 'Ben'
+# caltest.week[0][1] = 'Alex'
+# print(caltest.week[0])
 
 
-
+p1 = Provider("John", 1, 2, [1, 0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0, 1], [0, 0, 0, 0, 0, 0, 0], 40)
+p2 = Provider("Jane", 0, 1, [1, 0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0, 1], [0, 0, 0, 0, 0, 0, 0], 40)
+c1 = Clinic("Clinic1", 1, 2, 3)
+c2 = Clinic("Clinic2", 1, 2, 3)
 
 
