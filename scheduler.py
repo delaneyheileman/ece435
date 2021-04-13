5ad9af35c5417f88dda0b89110e4a046be79d50c
benjbwr <benjbwr@unm.edu>
2021-04-13 13:21:07 -0600

    added dummy data based on powerpoint; added clinic subclass and logic and counter functions; updated logic to use class functions; updated class attributes

--- scheduler.py
+++ scheduler.py
@@ -8,23 +8,81 @@ class Cal7:
 
 
 class Provider:
-    def __init__(self, provider_name, specialty, clinic_preference, day_preferences, days_off,
+    def __init__(self, provider_name, specialty, clinic_day_preference, days_off,
                  total_available_hours):
         self.provider_name = provider_name
-        self.specialty = specialty  # [Child,Adult,Family] Preferences will be indicated by 1
-        self.clinic_preference = clinic_preference  # [Clinic1,Clinic2,Clinic3] Preference will be indicated by 1
-        self.day_preferences = day_preferences  # [M,T,W,T,F,S,S] Preference will be indicated by 1
+        self.specialty = specialty  # Child = 0, Adult = 1, Family = 2
+        self.clinic_day_preference = clinic_day_preference  # Indicate clinic preference 1-3 on days 0-6; 0 for off day; 4 no preference
         self.days_off = days_off  # [M,T,W,T,F,S,S] Preference will be indicated by 1
         self.total_available_hours = total_available_hours
 
 
 class Clinic:
-    def __init__(self, clinic_name, min_staff, optimal_staff, max_staff, calendar):
+    def __init__(self, clinic_name, min_ped, min_aud, idl_ped, idl_aud, max_staff, calendar):
         self.clinic_name = clinic_name
-        self.min_staff = min_staff
-        self.optimal_staff = optimal_staff
+        self.min_ped = min_ped
+        self.min_aud = min_aud
+        self.idl_ped = idl_ped
+        self.idl_aud = idl_aud
         self.max_staff = max_staff
         self.calendar = calendar
+        self.ped_counter = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+        self.aud_counter = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+        self.max_counter = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+        self.specialty_list = [0, 0] * max_staff
+
+    def specialty_incrementer(self, specialty, d, s):
+        if specialty == 0:
+            self.ped_counter[d][s] += 1
+            self.max_counter[d][s] += 1
+        elif specialty == 1:
+            self.aud_counter[d][s] += 1
+            self.max_counter[d][s] += 1
+        else:
+            self.aud_counter[d][s] += 1
+            self.max_counter[d][s] += 1
+
+    def staff_logic(self, specialty, d, s):
+
+        if specialty == 0 and self.ped_counter[d][s] <= self.idl_ped and self.max_counter[d][s] <= self.max_staff:
+            self.specialty_incrementer(specialty, d, s)
+            return True
+        elif specialty == 1 and self.aud_counter[d][s] <= self.idl_aud and self.max_counter[d][s] <= self.max_staff:
+            self.specialty_incrementer(specialty, d, s)
+            return True
+        elif specialty == 2 and self.aud_counter[d][s] <= self.idl_aud and self.max_counter[d][s] <= self.max_staff:
+            self.specialty_incrementer(specialty, d, s)
+            return True
+        else:
+            return False
+
+
+class Satellite_clinic(Clinic):
+    def specialty_incrementer(self, specialty, d, s):
+        if specialty == 0:
+            self.ped_counter[d][s] += 1
+            self.max_counter[d][s] += 1
+        elif specialty == 1:
+            self.aud_counter[d][s] += 1
+            self.max_counter[d][s] += 1
+        else:
+            self.ped_counter[d][s] += 1
+            self.aud_counter[d][s] += 1
+            self.max_counter[d][s] += 1
+
+    def staff_logic(self, specialty, d, s):
+
+        if specialty == 0 and self.max_counter[d][s] < self.max_staff and self.aud_counter[d][s] >= self.min_aud:
+            self.specialty_incrementer(specialty, d, s)
+            return True
+        elif specialty == 1 and self.max_counter[d][s] < self.max_staff and self.ped_counter[d][s] >= self.min_ped:
+            self.specialty_incrementer(specialty, d, s)
+            return True
+        elif specialty == 2 and self.max_counter[d][s] < self.max_staff:
+            self.specialty_incrementer(specialty, d, s)
+            return True
+        else:
+            return False
 
 
 def printer(clnc):
@@ -40,39 +98,30 @@ def printer(clnc):
     print(out[2][1])
 
 
-p1 = Provider(",John", 0, 0, [[0, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p2 = Provider("Jane", 0, 0, [[0, 0], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p3 = Provider("Jill", 0, 0, [[1, 0], [1, 0], [1, 0], [1, 0], [0, 0], [0, 0], [1, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p4 = Provider("Jake", 0, 0, [[1, 0], [1, 0], [0, 0], [0, 0], [1, 0], [1, 0], [1, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p5 = Provider("Ben", 0, 0, [[1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
-p6 = Provider("Alex", 0, 0, [[0, 1], [0, 1], [0, 1], [0, 1], [0, 0], [0, 0], [0, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
-p7 = Provider("Derek", 0, 0, [[0, 1], [0, 1], [0, 0], [0, 0], [0, 1], [0, 1], [0, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
+# def __init__(self, provider_name, specialty, clinic_day_preference, days_off, total_available_hours):
 
-p8 = Provider("Delaney", 0, 1, [[0, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p9 = Provider("Janner", 0, 1, [[0, 0], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p10 = Provider("Alejandro", 0, 1, [[1, 0], [1, 0], [1, 0], [1, 0], [0, 0], [0, 0], [1, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p11 = Provider("Billy", 0, 1, [[1, 0], [1, 0], [0, 0], [0, 0], [1, 0], [1, 0], [1, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p12 = Provider("Bob", 0, 1, [[1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
-p13 = Provider("Margaret", 0, 1, [[0, 1], [0, 1], [0, 1], [0, 1], [0, 0], [0, 0], [0, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
-p14 = Provider("Adam", 0, 1, [[0, 1], [0, 1], [0, 0], [0, 0], [0, 1], [0, 1], [0, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
-
-p15 = Provider("Dennis", 0, 2, [[0, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p16 = Provider("Anne", 0, 2, [[0, 0], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p17 = Provider("Ryan", 0, 2, [[1, 0], [1, 0], [1, 0], [1, 0], [0, 0], [0, 0], [1, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p18 = Provider("Charleston", 0, 2, [[1, 0], [1, 0], [0, 0], [0, 0], [1, 0], [1, 0], [1, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
-p19 = Provider("Nathan", 0, 2, [[1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
-p20 = Provider("Connor", 0, 2, [[0, 1], [0, 1], [0, 1], [0, 1], [0, 0], [0, 0], [0, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
-p21 = Provider("Anita", 0, 2, [[0, 1], [0, 1], [0, 0], [0, 0], [0, 1], [0, 1], [0, 1]], [0, 0, 0, 0, 0, 0, 0], 40)
+p1 = Provider("Colgan", 0, [[0, 0], [1, 1], [1, 1], [0, 0], [0, 0], [1, 1], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
+p2 = Provider("Jones", 0, [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
+p3 = Provider("Zelleke", 0, [[0, 0], [0, 0], [0, 0], [0, 1], [1, 1], [2, 2], [0, 0]], [0, 1, 1, 0, 0, 0, 0], 40)
+p4 = Provider("Fraser", 2, [[0, 0], [0, 0], [0, 1], [0, 0], [0, 1], [1, 1], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
+p5 = Provider("Garza", 2, [[0, 0], [1, 1], [1, 0], [0, 1], [1, 0], [0, 0], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
+p6 = Provider("Haley", 2, [[0, 0], [2, 2], [2, 2], [0, 2], [0, 0], [0, 0], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
+p7 = Provider("Veal", 2, [[0, 0], [3, 3], [3, 3], [0, 3], [3, 3], [0, 0], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
+p8 = Provider("Wilkerson", 2, [[0, 0], [0, 0], [0, 1], [0, 0], [1, 0], [1, 1], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
+p9 = Provider("Littlejohn", 2, [[0, 0], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
+p10 = Provider("Willie", 1, [[0, 0], [0, 0], [2, 2], [0, 0], [0, 0], [0, 0], [0, 0]], [0, 0, 0, 0, 0, 0, 0], 40)
 
 cc1 = np.empty(shape=(7, 2, 3), dtype='object')
 cc2 = np.empty(shape=(7, 2, 3), dtype='object')
 cc3 = np.empty(shape=(7, 2, 3), dtype='object')
-c1 = Clinic(0, 2, 5, 5, cc1)
-c2 = Clinic(1, 1, 2, 3, cc2)
-c3 = Clinic(2, 1, 2, 3, cc3)
-# print(p1.provider_name,p1.specialty,p1.clinic_preference,p1.am_day_preferences[0], p1.calendar_am)
 
-Provider_List = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21]
+# def __init__(self, clinic_name, min_ped, min_aud, idl_ped, idl_aud, max_staff, calendar):
+c1 = Clinic(1, 1, 1, 2, 3, 5, cc1)
+c2 = Satellite_clinic(2, 1, 1, 3, 3, 3, cc2)
+c3 = Satellite_clinic(3, 1, 1, 3, 3, 3, cc3)
+
+
+Provider_List = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
 Clinic_List = [c1, c2, c3]
 
 # provider location preference
@@ -102,13 +151,13 @@ for day in weekdays:
         for shift in shifts:
             for slot in slots:
                 for provider in Provider_List:
-                    if provider.clinic_preference == clinic.clinic_name:
-                        if provider.day_preferences[day][shift] == 1:
-                            if provider.days_off[day] == 0:
-                                if provider.total_available_hours >= 8:
-                                    if clinic.calendar[day][shift][slot] is None and provider.provider_name not in\
-                                            clinic.calendar[day][shift]:
-                                        provider.total_available_hours = provider.total_available_hours - 8
+                    if provider.clinic_day_preference[day][shift] == clinic.clinic_name:
+                        if provider.days_off[day] == 0:
+                            if provider.total_available_hours >= 4:
+                                if clinic.calendar[day][shift][slot] is None and provider.provider_name not in \
+                                        clinic.calendar[day][shift]:
+                                    if clinic.staff_logic(provider.specialty, day, shift):
+                                        provider.total_available_hours = provider.total_available_hours - 4
                                         clinic.calendar[day][shift][slot] = provider.provider_name
 
 print("\n")
