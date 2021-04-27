afc84aa15a9f39c77726aa330ad4339e2b43b8df
benjbwr <benjbwr@unm.edu>
2021-04-26 19:47:54 -0600

    comments added to scheduler

--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,8 @@ import pandas as pd
 import holidays
 import datetime
 
+# Date inputs for calendar generator
+
 year = 2021
 month_start = 1
 month_end = 1
@@ -10,14 +12,20 @@ day_start = 17
 day_end = 23
 
 
-def calendar_generator(Year, Start_Month, End_Month, Start_Day, End_Day):
-    us_holidays = []
+# generates a calender based on inputs above
 
+def calendar_generator(Year, Start_Month, End_Month, Start_Day, End_Day):
+    us_holidays = []  # array for holidays
+
+    # finds federal holidays in specified year
     for date in holidays.UnitedStates(years=Year).items():
         us_holidays.append(str(date[0]))
 
+    # creates a time frame based on inputs
     start_date = datetime.datetime(year=Year, month=Start_Month, day=Start_Day)
     end_date = datetime.datetime(year=Year, month=End_Month, day=End_Day)
+
+    # creates data frame and adds holidays to time frame. Final result is a two week period with federal holidays
     date_frame = pd.DataFrame()
     date_frame["Dates"] = pd.date_range(start_date, end_date)
     date_frame.head()
@@ -26,28 +34,38 @@ def calendar_generator(Year, Start_Month, End_Month, Start_Day, End_Day):
     return date_frame
 
 
+# class object of providers.
+
 class Provider:
     def __init__(self, provider_name, specialty, day_preferences, total_available_hours):
         self.provider_name = provider_name
         self.specialty = specialty
-        self.day_preferences = day_preferences
-        self.total_available_hours = total_available_hours
-        self.week = np.empty(shape=(7, 2), dtype="object")
+        self.day_preferences = day_preferences  # hold where and when a provider wants to work
+        self.total_available_hours = total_available_hours  # shift counter
+        self.week = np.empty(shape=(7, 2), dtype="object")  # provider centric schedule days, shifts
+
+
+# class object of clinic (Crown point clinic)
 
 
 class Clinic:
     def __init__(self, clinic_name, min_ped, min_aud, idl_ped, idl_aud, max_staff):
         self.clinic_name = clinic_name
+        # minimum requirements
         self.min_ped = min_ped
         self.min_aud = min_aud
+        # ideal staff amounts
         self.idl_ped = idl_ped
         self.idl_aud = idl_aud
         self.max_staff = max_staff
+        # counters for each specialty on each shift and day
         self.ped_counter = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
         self.aud_counter = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
         self.max_counter = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-        self.week = np.empty(shape=(7, 2, 3), dtype="object")
+        # clinic centric calendar days,shifts,amount of slots
+        self.week = np.empty(shape=(7, 2, max_staff), dtype="object")
 
+    # this class function increments clinic counters based on specialties
     def specialty_incrementer(self, specialty, d, s):
         if specialty == "PED":
             self.ped_counter[d][s] += 1
@@ -59,6 +77,7 @@ class Clinic:
             self.aud_counter[d][s] += 1
             self.max_counter[d][s] += 1
 
+    # this class functions checks the clinic counter and the specialty of a provider to see if assignment is possible
     def staff_logic(self, specialty, d, s):
 
         if specialty == "PED" and self.ped_counter[d][s] <= self.idl_ped and self.max_counter[d][s] <= self.max_staff:
@@ -74,7 +93,10 @@ class Clinic:
             return False
 
 
+# clinic subclass for sub clinics (THS PPHC)
 class Satellite_clinic(Clinic):
+
+    # increments PED and AUD for family practice instead of just AUD
     def specialty_incrementer(self, specialty, d, s):
         if specialty == "PED":
             self.ped_counter[d][s] += 1
@@ -87,6 +109,7 @@ class Satellite_clinic(Clinic):
             self.aud_counter[d][s] += 1
             self.max_counter[d][s] += 1
 
+    # staff logic for sub clinics
     def staff_logic(self, specialty, d, s):
 
         if specialty == "PED" and self.max_counter[d][s] < self.max_staff and self.aud_counter[d][s] >= self.min_aud:
@@ -129,7 +152,6 @@ calendar = calendar_generator(year, month_start, month_end, day_start, day_end)
 
 
 def scheduler(Provider_List, Clinic_List):
-
     weekdays = [0, 1, 2, 3, 4, 5, 6]
     shifts = [0, 1]
     slots = [0, 1, 2]
@@ -147,6 +169,7 @@ def scheduler(Provider_List, Clinic_List):
                             if provider.total_available_hours >= 4:
                                 if clinic.week[day][shift][slot] is None and provider.provider_name not in \
                                         clinic.week[day][shift]:
+
                                     if clinic.staff_logic(provider.specialty, day, shift):
                                         provider.total_available_hours = provider.total_available_hours - 4
                                         clinic.week[day][shift][slot] = provider.provider_name
