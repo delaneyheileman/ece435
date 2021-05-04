e170d747e76cd78cc923d2d7707ed33d07891e07
Derek Davidson <davidson.derek@gmail.com>
2021-05-04 17:54:46 -0600

    Add find_next_monday()

--- scheduler.py
+++ scheduler.py
@@ -11,6 +11,15 @@ month_end = 1
 day_start = 17
 day_end = 23
 
+def find_next_monday():
+    today = datetime.date.today()
+    if (today.weekday() == 0):
+        return today
+    else:
+        for d in range(7):
+            day = today +datetime.timedelta(days=d)
+            if (day.weekday() == 0):
+                return day
 
 # generates a calender based on inputs above
 
